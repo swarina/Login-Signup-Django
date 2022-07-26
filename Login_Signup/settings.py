cf8cc59f0cb7099fcfbc04628757eb7a71b127fe
Swarina Jaiswal <swarinajaiswal@gmail.com>
2022-07-26 19:29:28 +0530

    Fixes

--- Login_Signup/settings.py
+++ Login_Signup/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
